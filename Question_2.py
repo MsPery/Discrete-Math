c7a66fedbc61d31c0c229645947746a2cfb52e30
MercyNyamusi <m.nyamusi@alustudent.com>
2021-11-10 19:00:35 +0200

    Group Assignment

--- Question_2.py
+++ Question_2.py
@@ -3,18 +3,15 @@
 # the set A − B,
 # the set A × B.
 
-# noinspection PyUnresolvedReferences
 
 import itertools
 
-set_A = {"1", "2", "3", "4", "5", "6", "7", "8"}  # A set of natural number less that 10
-set_B = {"2", "4", "6", "8"}  # A set of positive integers that are divisible by 2 and they are less than 10
-print(sorted(set_A))
-print(sorted(set_B))
+set_A = {1, 2, 3, 4, 5, 6, 7, 8}  # A set of natural number less that 10
+set_B = {2, 4, 6, 8}  # A set of positive integers that are divisible by 2 and they are less than 10
+
 print("B ⊆ A:", set_B.issubset(set_A))
-print("A-B:", sorted(set_A.difference(set_B)))
+print("A-B =", sorted(set_A.difference(set_B)))
+list1 = []
 for cartesianProduct in itertools.product(set_A, set_B):
-   print(cartesianProduct)
-
-
-
+    list1.append(cartesianProduct)
+print("AXB =", sorted(list1))
